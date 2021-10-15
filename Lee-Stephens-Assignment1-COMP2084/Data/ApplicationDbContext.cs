c7d15e9cc7587a6a86c1b391770b337fafff27bf
leestephens12 <77406283+leestephens12@users.noreply.github.com>
2021-10-14 22:34:22 -0400

    Add project files.

---
+++ Lee-Stephens-Assignment1-COMP2084/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lee_Stephens_Assignment1_COMP2084.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Lee-Stephens-Assignment1-COMP2084/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lee_Stephens_Assignment1_COMP2084.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
